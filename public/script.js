d4b784cca234b39c07c11634c40ebdd1c4ba87b5
Isaac Hatton <IsaacHatton@outlook.com>
2023-10-19 15:20:07 +0000

    Basic userAgentData.platform hints

--- public/script.js
+++ public/script.js
@@ -17,3 +17,53 @@ if ("serviceWorker" in navigator) {
       console.error(err);
     });
 }
+
+function userAgentDetectDevice(){
+  return ""
+}
+
+function detectDevice(){
+  // try to detect using navigator.userAgentData.platform
+  try{
+    // windows
+    if(navigator.userAgentData.platform === "Windows"){
+      return "Windows"
+    }
+    // macOS
+    else if(navigator.userAgentData.platform === "MacIntel"){
+      return "macOS"
+    }
+    // linux
+    else if(navigator.userAgentData.platform === "Linux x86_64"){
+      return "Linux"
+    }
+    // android
+    else if(navigator.userAgentData.platform === "Android"){
+      return "Android"
+    }
+    // ios
+    else if(navigator.userAgentData.platform === "iPhone" || navigator.userAgentData.platform === "iPad" || navigator.userAgentData.platform === "iPod"){
+      return "iOS"
+    }else{
+      console.log("navigator.userAgentData.platform didn't return a supported value")
+      return userAgentDetectDevice()
+    }
+  }catch(e){
+    console.log("navigator.userAgentData.platform not supported")
+    return userAgentDetectDevice()
+  }
+}
+
+// if detectDeviceMessage element exists
+if (document.getElementById("detectDeviceMessage")) {
+  // Detect device
+  const device = detectDevice();
+  if(device === ""){
+    console.log("detectDevice() retruned an empty string")
+  }else{
+    // Get detectDeviceMessage element
+    const detectDeviceMessage = document.getElementById("detectDeviceMessage");
+    // Set detectDeviceMessage element text
+    detectDeviceMessage.textContent = "You are using " + device;
+  }
+}
