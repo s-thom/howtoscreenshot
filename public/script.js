5ab80761bf942ffc543fa9e158d41ff441d09adf
Isaac Hatton <IsaacHatton@outlook.com>
2023-10-21 06:48:23 +0000

    run prettier

--- public/script.js
+++ public/script.js
@@ -18,33 +18,37 @@ if ("serviceWorker" in navigator) {
     });
 }
 
-function userAgentDetectDevice(){
+function userAgentDetectDevice() {
   // if the userAgent contains "Windows"
-  if(navigator.userAgent.indexOf("Windows") !== -1){
-    return "Windows"
+  if (navigator.userAgent.indexOf("Windows") !== -1) {
+    return "Windows";
   }
   // if the userAgent contains "Macintosh"
-  else if(navigator.userAgent.indexOf("Macintosh") !== -1){
-    return "macOS"
+  else if (navigator.userAgent.indexOf("Macintosh") !== -1) {
+    return "macOS";
   }
   // if the userAgent contains "Linux"
-  else if(navigator.userAgent.indexOf("Linux") !== -1){
-    return "Linux"
+  else if (navigator.userAgent.indexOf("Linux") !== -1) {
+    return "Linux";
   }
   // if the userAgent contains "Android"
-  else if(navigator.userAgent.indexOf("Android") !== -1){
-    return "Android"
+  else if (navigator.userAgent.indexOf("Android") !== -1) {
+    return "Android";
   }
   // if the userAgent contains "iPhone" or "iPad" or "iPod"
-  else if(navigator.userAgent.indexOf("iPhone") !== -1 || navigator.userAgent.indexOf("iPad") !== -1 || navigator.userAgent.indexOf("iPod") !== -1){
-    return "iOS"
-  }else{
-    console.log("navigator.userAgent didn't return a supported value")
-    return ""
+  else if (
+    navigator.userAgent.indexOf("iPhone") !== -1 ||
+    navigator.userAgent.indexOf("iPad") !== -1 ||
+    navigator.userAgent.indexOf("iPod") !== -1
+  ) {
+    return "iOS";
+  } else {
+    console.log("navigator.userAgent didn't return a supported value");
+    return "";
   }
 }
 
-function detectDevice(){
+function detectDevice() {
   // if navigator.userAgentData is not supported
   if (!(navigator.userAgentData && navigator.userAgentData.platform)) {
     return userAgentDetectDevice();
@@ -54,29 +58,31 @@ function detectDevice(){
       // windows
       case "Windows":
         // Windows-specific code here
-        return "Windows"
+        return "Windows";
       // macOS
       case "MacIntel":
         // macOS-specific code here
-        return "macOS"
+        return "macOS";
       // linux
       case "Linux x86_64":
         // Linux-specific code here
-        return "Linux"
+        return "Linux";
       // android
       case "Android":
         // Android-specific code here
-        return "Android"
+        return "Android";
       // ios
       case "iPhone":
       case "iPad":
       case "iPod":
         // iOS-specific code here
-        return "iOS"
-      
+        return "iOS";
+
       default:
-        console.log("navigator.userAgentData.platform didn't return a supported value")
-        return userAgentDetectDevice()
+        console.log(
+          "navigator.userAgentData.platform didn't return a supported value",
+        );
+        return userAgentDetectDevice();
     }
   }
 }
@@ -85,9 +91,9 @@ function detectDevice(){
 if (document.getElementById("detectDeviceMessage")) {
   // Detect device
   const device = detectDevice();
-  if(device === ""){
-    console.log("detectDevice() returned an empty string")
-  }else{
+  if (device === "") {
+    console.log("detectDevice() returned an empty string");
+  } else {
     // Get detectDeviceMessage element
     const detectDeviceMessage = document.getElementById("detectDeviceMessage");
     // Set detectDeviceMessage element text
