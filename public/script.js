d402b5b0e3a86da6b78e7e9102973fc25b663fd2
Stuart Thomson <me@sthom.kiwi>
2024-06-23 10:14:48 +1200

    Remove navigation click tracking and reformat files

--- public/script.js
+++ public/script.js
@@ -24,66 +24,65 @@ function userAgentDetectDevice() {
     return "Windows";
   }
   // if the userAgent contains "Macintosh"
-  else if (navigator.userAgent.indexOf("Macintosh") !== -1) {
+  if (navigator.userAgent.indexOf("Macintosh") !== -1) {
     return "macOS";
   }
   // if the userAgent contains "Linux"
-  else if (navigator.userAgent.indexOf("Linux") !== -1) {
+  if (navigator.userAgent.indexOf("Linux") !== -1) {
     return "Linux";
   }
   // if the userAgent contains "Android"
-  else if (navigator.userAgent.indexOf("Android") !== -1) {
+  if (navigator.userAgent.indexOf("Android") !== -1) {
     return "Android";
   }
   // if the userAgent contains "iPhone" or "iPad" or "iPod"
-  else if (
+  if (
     navigator.userAgent.indexOf("iPhone") !== -1 ||
     navigator.userAgent.indexOf("iPad") !== -1 ||
     navigator.userAgent.indexOf("iPod") !== -1
   ) {
     return "iOS";
-  } else {
-    console.log("navigator.userAgent didn't return a supported value");
-    return "";
   }
+
+  console.log("navigator.userAgent didn't return a supported value");
+  return "";
 }
 
 function detectDevice() {
   // if navigator.userAgentData is not supported
   if (!(navigator.userAgentData && navigator.userAgentData.platform)) {
     return userAgentDetectDevice();
-  } else {
-    switch (navigator.userAgentData.platform) {
-      // try to detect using navigator.userAgentData.platform
-      // windows
-      case "Windows":
-        // Windows-specific code here
-        return "Windows";
-      // macOS
-      case "MacIntel":
-        // macOS-specific code here
-        return "macOS";
-      // linux
-      case "Linux x86_64":
-        // Linux-specific code here
-        return "Linux";
-      // android
-      case "Android":
-        // Android-specific code here
-        return "Android";
-      // ios
-      case "iPhone":
-      case "iPad":
-      case "iPod":
-        // iOS-specific code here
-        return "iOS";
+  }
+  switch (navigator.userAgentData.platform) {
+    // try to detect using navigator.userAgentData.platform
+    // windows
+    case "Windows":
+      // Windows-specific code here
+      return "Windows";
+    // macOS
+    case "MacIntel":
+      // macOS-specific code here
+      return "macOS";
+    // linux
+    case "Linux x86_64":
+      // Linux-specific code here
+      return "Linux";
+    // android
+    case "Android":
+      // Android-specific code here
+      return "Android";
+    // ios
+    case "iPhone":
+    case "iPad":
+    case "iPod":
+      // iOS-specific code here
+      return "iOS";
 
-      default:
-        console.log(
-          "navigator.userAgentData.platform didn't return a supported value",
-        );
-        return userAgentDetectDevice();
-    }
+    default:
+      console.log(
+        "navigator.userAgentData.platform didn't return a supported value",
+      );
+      return userAgentDetectDevice();
   }
 }
 
